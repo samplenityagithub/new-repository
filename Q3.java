649d095096a1927fb209661dce0c39aa1d9ec2a3
Nitya Singh <101619574+samplenityagithub@users.noreply.github.com>
2022-03-28 10:30:19 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class Q3 {
+	
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner s= new Scanner(System.in);
+		System.out.println("enter first number");
+		a=s.nextInt();
+		System.out.println("enter second number");
+		b=s.nextInt();
+
+int sum=a+b;
+		System.out.println("sum is "+sum);
+		
+	}
+
+}
